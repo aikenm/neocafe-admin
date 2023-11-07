eb31c60643d13864daae78c30be0bd842686a64c
aikenm <a.mnsbkv@gmail.com>
2023-11-08 00:52:22 +0600

    Implemented subpages and navigation bteween subpages

--- src/pages/main_subpages/Branches.jsx
+++ src/pages/main_subpages/Branches.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
+import '../../styles/pages/subpages/branches.css';
 
 const Branches = () => {
     const handleBranchesSearch = (searchTerm) => {
@@ -10,7 +11,23 @@ const Branches = () => {
     <div className="branches-container">
       <ContentHeader title="Филиалы" onSearch={handleBranchesSearch} />
       <div className="branches-content">
-        
+        <div className='branches-content-header'>
+                <span className='branches-content-header-subtitle'>
+                    №
+                </span>
+                <span className='branches-content-header-subtitle'>
+                    Название кофейни
+                </span>
+                <span className='branches-content-header-subtitle'>
+                    Адрес
+                </span>
+                <span className='branches-content-header-subtitle'>
+                    Время работы
+                </span>
+                <span className='branches-content-header-subtitle'>
+                    Ред.
+                </span>
+            </div>
       </div>
     </div>
   );
--- src/pages/main_subpages/Menu.jsx
+++ src/pages/main_subpages/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
+import '../../styles/pages/subpages/menu.css';
 
 const Menu = () => {
     const handleMenuSearch = (searchTerm) => {
@@ -10,6 +11,26 @@ const Menu = () => {
     <div className="menu-container">
       <ContentHeader title="Меню" onSearch={handleMenuSearch} />
       <div className="menu-content">
+        <div className='menu-content-header'>
+            <span className='menu-content-header-subtitle'>
+                №
+            </span>
+            <span className='menu-content-header-subtitle'>
+                Наименование
+            </span>
+            <span className='menu-content-header-subtitle'>
+                Категория
+            </span>
+            <span className='menu-content-header-subtitle'>
+                Состав блюда и граммовка
+            </span>
+            <span className='menu-content-header-subtitle'>
+                Стоимость
+            </span>
+            <span className='menu-content-header-subtitle'>
+                Филиал
+            </span>
+        </div>
       </div>
     </div>
   );
