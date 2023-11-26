c6105a7dac8953401f2188ab768583d59036f060
manasbekov <manasbekov_a@auca.kg>
2023-11-26 23:52:43 +0600

    Implemented drop down for branch choosing in stock

--- src/components/ContentHeader.jsx
+++ src/components/ContentHeader.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import '../styles/components/content_header.css'
 
-const ContentHeader = ({ title, onCreate, searchTerm, onSearch }) => {
+// ContentHeader.jsx
+const ContentHeader = ({ title, onCreate, searchTerm, onSearch, stocks, selectedStock, onSelectStock }) => {
   return (
     <div className="content-header">
-      <h1 className='content-header-title'>{title}</h1>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Поиск"
-          value={searchTerm}
-          onChange={(e) => onSearch(e.target.value)}
-          className='search-bar'
-        />
-        <button onClick={onCreate} className='create-button'>Создать</button>
+      <div className='header-wrapper'>
+        <h1 className='content-header-title'>{title}</h1>
+        {/* Dropdown for selecting stock */}
+          {stocks && onSelectStock && (
+            <select value={selectedStock} onChange={(e) => onSelectStock(e.target.value)} className='stock-select'>
+              {stocks.map(stock => (
+                <option key={stock.id} value={stock.id}>{stock.name}</option>
+              ))}
+            </select>
+          )}
       </div>
-    </div>
+
+      {/* Search container */}
+      <div className="search-container">
+          <input
+            type="text"
+            placeholder="Поиск"
+            value={searchTerm}
+            onChange={(e) => onSearch(e.target.value)}
+            className='search-bar'
+          />
+          <button onClick={onCreate} className='create-button'>Создать</button>
+        </div>
+      </div>
   );
 };
 
 export default ContentHeader;
+
--- src/pages/main_subpages/Stock.jsx
+++ src/pages/main_subpages/Stock.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+// Stock.jsx
+import React, { useState } from 'react';
 import ContentHeader from '../../components/ContentHeader';
 
 const Stock = () => {
-    const handleStockSearch = (searchTerm) => {
-        
-    };
+  const [selectedStock, setSelectedStock] = useState('stock1');
+  const [items, setItems] = useState([]); // This will store items for the selected stock
+
+  // Sample data for stocks
+  const stocks = [
+    { id: 'stock1', name: 'Stock 1' },
+    { id: 'stock2', name: 'Stock 2' },
+    { id: 'stock3', name: 'Stock 3' },
+    // Add more stocks as needed
+  ];
+
+  const handleStockSearch = (searchTerm) => {
+    // Implement search logic
+  };
+
+  const handleSelectStock = (stockId) => {
+    setSelectedStock(stockId);
+    // Fetch items for the selected stock and update `items`
+  };
 
   return (
     <div className="stock-container">
-      <ContentHeader title="Склад" onSearch={handleStockSearch} />
+      <ContentHeader 
+        title="Склад"
+        onSearch={handleStockSearch}
+        stocks={stocks}
+        selectedStock={selectedStock}
+        onSelectStock={handleSelectStock}
+      />
       <div className="stock-content">
+        {/* Render items for the selected stock */}
       </div>
     </div>
   );
